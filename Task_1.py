066842ff568e8f096ec15a6c55ddeb1299a51ec9
Parth-Gori-1 <parthgori116@gmail.com>
2026-01-09 12:21:23 +0530

    Initial commit

---
+++ Task_1.py
@@ -0,0 +1,12 @@
+
+# Task 1: Check if a Number is Even or Odd
+# Beginner Python Program
+
+# Taking input from the user
+number = int(input("Enter a number: "))
+
+# Checking if the number is even or odd
+if number % 2 == 0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
