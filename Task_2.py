73f8974a842ac472efd2b8eec1d9ece4b1dcefaa
Parth-Gori-1 <parthgori116@gmail.com>
2026-01-09 12:27:53 +0530

    Updated Code

--- Task_2.py
+++ Task_2.py
@@ -1,12 +1,7 @@
-
 # Task 2: Sum of numbers from 1 to 50
-# Beginner Python Program
-
 sum = 0
 
-# Loop from 1 to 50
 for i in range(1, 51):
     sum = sum + i
 
-# Displaying the result
 print("The sum of numbers from 1 to 50 is:", sum)
